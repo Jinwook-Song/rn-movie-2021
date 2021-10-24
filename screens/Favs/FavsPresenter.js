13ab489a401437c684f2321eeb13f54904462861
jinwook-song <wlsdnr129@naver.com>
2021-10-24 15:16:06 +0900

    moving cards

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions, PanResponder } from "react-native";
+import { Animated, Dimensions, PanResponder } from "react-native";
 import styled from "styled-components/native";
 import { apiImage } from "../../api";
 import { theme } from "../../colors";
@@ -11,31 +11,41 @@ const Container = styled.View`
   background-color: ${theme.black};
   align-items: center;
 `;
-const Card = styled.View`
-  height: ${HEIGHT / 1.5}px;
-  width: 90%;
-  position: absolute;
-  top: 80px;
-`;
+const Card = styled.View``;
 const Poster = styled.Image`
   width: 100%;
   height: 100%;
   border-radius: 20px;
 `;
 
+const styles = {
+  top: 80,
+  height: HEIGHT / 1.5,
+  width: "90%",
+  position: "absolute",
+};
+
 export default ({ results }) => {
+  const position = new Animated.ValueXY();
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
-    onPanResponderMove: (event, { dx }) => {
-      console.log(dx);
+    onPanResponderMove: (event, { dx, dy }) => {
+      position.setValue({ x: dx, y: dy });
     },
   });
   return (
     <Container>
       {results?.reverse().map((result) => (
-        <Card key={result.id} {...panResponder.panHandlers}>
+        <Animated.View
+          style={{
+            ...styles,
+            transform: [...position.getTranslateTransform()],
+          }}
+          key={result.id}
+          {...panResponder.panHandlers}
+        >
           <Poster source={{ uri: apiImage(result.poster_path) }} />
-        </Card>
+        </Animated.View>
       ))}
     </Container>
   );
