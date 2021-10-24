96053d5c22c227be0e79ed0689931bd7c3420c02
jinwook-song <wlsdnr129@naver.com>
2021-10-24 15:46:56 +0900

    Realeasing TopCard

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Animated, Dimensions, PanResponder } from "react-native";
 import styled from "styled-components/native";
 import { apiImage } from "../../api";
@@ -19,34 +19,61 @@ const Poster = styled.Image`
 `;
 
 const styles = {
-  top: 80,
+  top: 70,
   height: HEIGHT / 1.5,
   width: "90%",
   position: "absolute",
 };
 
 export default ({ results }) => {
+  const [topCover, setTopConver] = useState(0);
   const position = new Animated.ValueXY();
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (event, { dx, dy }) => {
       position.setValue({ x: dx, y: dy });
     },
+    onPanResponderRelease: () => {
+      Animated.spring(position, {
+        toValue: {
+          x: 0,
+          y: 0,
+        },
+        useNativeDriver: true,
+        bounciness: 20,
+      }).start();
+    },
   });
   return (
     <Container>
-      {results?.reverse().map((result) => (
-        <Animated.View
-          style={{
-            ...styles,
-            transform: [...position.getTranslateTransform()],
-          }}
-          key={result.id}
-          {...panResponder.panHandlers}
-        >
-          <Poster source={{ uri: apiImage(result.poster_path) }} />
-        </Animated.View>
-      ))}
+      {results?.map((result, index) => {
+        if (index === topCover) {
+          return (
+            <Animated.View
+              style={{
+                ...styles,
+                zIndex: 1,
+                transform: [...position.getTranslateTransform()],
+              }}
+              key={result.id}
+              {...panResponder.panHandlers}
+            >
+              <Poster source={{ uri: apiImage(result.poster_path) }} />
+            </Animated.View>
+          );
+        }
+        return (
+          <Animated.View
+            style={{
+              ...styles,
+            }}
+            key={result.id}
+            {...panResponder.panHandlers}
+          >
+            <Poster source={{ uri: apiImage(result.poster_path) }} />
+          </Animated.View>
+        );
+      })}
     </Container>
   );
 };
